1b841d6417711be2f9fadce4e24e1adf8154c536
YeerayRN <161733740+YeerayRN@users.noreply.github.com>
2024-10-17 21:06:30 +0200

    Add files via upload

--- morphing.cpp
+++ morphing.cpp
@@ -7,16 +7,14 @@ Video Morphing(const Image &I1,const Image &I2,int nframes){
  //RELLENAR POR EL ESTUDIANTE
 
     Video video(nframes);
+    Image frame(I1);
 
     for(int i = 0; i < nframes; i++){
-        Image frame;
         for(int j = 0; j < I1.get_rows(); j++){
             for(int k = 0; k < I1.get_cols(); k++){
-                while(I1.get_pixel(i,j) != I2.get_pixel(i,j)){
-                    int op1 = I1.get_pixel(j,k) * (1 - i/nframes);
-                    int op2 = I2.get_pixel(j,k) * i/nframes;
-                    frame.set_pixel(j,k,op1 + op2);
-                }
+                int op1 = I1.get_pixel(j,k) * (1 - ((float)i/nframes));
+                int op2 = I2.get_pixel(j,k) * (float)i/nframes;
+                frame.set_pixel(j,k,op1 + op2);
             }
         }
         video.Insertar(i,frame);
