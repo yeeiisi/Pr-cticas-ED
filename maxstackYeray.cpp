e640f4100de118cb6e32ea1415e5b792e778fa70
YeerayRN <161733740+YeerayRN@users.noreply.github.com>
2024-11-13 13:28:58 +0100

    Add files via upload

--- maxstackYeray.cpp
+++ maxstackYeray.cpp
@@ -2,56 +2,46 @@
 @file maxstack.cpp
 @brief fichero con funciones primitivas de la clase MaxStack
 */
-#include "maxstack.h"
+#include "../include/maxstackYeray.h"
 
 MaxStack::MaxStack(){}
 
 void MaxStack::push(int elemento){
-    element almacenar;
-    almacenar.value = elemento;
-    cola.push(elemento);
+    element dato;
+    dato.value = elemento;
+    pair<element,int> almacenar; 
 
-    if(pila.empty()){
-        almacenar.maximum = elemento;
-        pila.push(almacenar);
+    if(cola.empty()){
+        dato.maximum = elemento;
+        almacenar.first = dato;
+        almacenar.second = 1;
+        cola.push(almacenar);
     }
     else{
-        if(elemento >= cola.top()){
-            almacenar.maximum = elemento;
+        if(elemento >= cola.top().first.maximum){
+            dato.maximum = elemento;
+            
         }
         else{
-            almacenar.maximum = cola.top();
+            dato.maximum = cola.top().first.maximum;
         }
 
-        pila.push(almacenar);
+        almacenar.first = dato;
+        almacenar.second = (cola.top().second + 1);
+        cola.push(almacenar);
     }
 }
 
-element& MaxStack::top(){
-    return pila.top();
+const element& MaxStack::top(){
+    return cola.top().first;
 }
 
 void MaxStack::pop(){
-    if(pila.top().value == cola.top()){
-        cola.pop();
-    }
-    else{
-        stack<int> auxiliar;
-        while(!cola.empty() && (pila.top().value != cola.top())){
-            auxiliar.push(cola.top());
-            cola.pop();
-        }
-        cola.pop();
-        while(!auxiliar.empty()){
-            cola.push(auxiliar.top());
-            auxiliar.pop();
-        }
-    }
-    pila.pop();
+    cola.pop();
 }
 
 bool MaxStack::empty(){
-    return pila.empty(); 
+    return cola.empty(); 
 }
 
 int MaxStack::size(){
--- maxstackYeray.h
+++ maxstackYeray.h
@@ -1,7 +1,6 @@
 #ifndef PRACTICA4_MAXSTACK
 #define PRACTICA4_MAXSTACK
 #include <iostream>
-#include <stack>
 #include <queue>
 using namespace std;
 
@@ -16,21 +15,95 @@ int value; // Current value to store
 int maximum; // Current max value in the structure
 };
 
+/**
+ * @brief Struct con un functor que usará la cola con prioridad para ordenar los elementos en su interior.
+ * Compara los int de los pairs, los cuales representan el orden de inserción en la cola.
+ * @param primero: Pair que contiene un element y un int
+ * @param segundo: Pair de element e int para comparar con el primero
+ * @return TRUE si el second del primero es menor, FALSE si es mayor
+ */
+struct Comparar{
+    bool operator()(pair<element,int> primero, pair<element,int> segundo){
+        if(primero.second < segundo.second)
+            return true;
+        else
+            return false;
+    }
+};
+
+/**
+  @brief T.D.A. MaxStack
+
+  Una instancia del tipo de dato abstracto MaxStack nos permite almacenar datos de tipo element, comportándose como una pila y diciendo cuál es el máximo de los introducidos en cada momento
+
+  El TDA ofrece los mismos métodos que una pila, pudiendo añadir, consultar el último añadido, quitar un elemento y consultar si está vacía y su tamaño
+
+  El formato de los elementos introducidos es: elemento,máximo
+  Para poder usar el TDA MaxStack se debe incluir el fichero
+
+  \#include <maxstack.h>
+
+  @author José Carlos y Yeray
+  @date Noviembre 2024
+
+**/
+
 class MaxStack{
+/**
+      @page repMaxStack Representación del TDA MaxStack .
+
+      @section invMaxStack Invariante de la representación.
+
+      Un objeto válido 1 del TDA MaxStack debe cumplir
+        -cola.top().first.element = int
+        -cola contiene un pair<element,int> 
+
+      @section faMaxStack Función de abstracción
+
+      Un objeto válido 1 del TDA MaxStack representa una pila que contiene datos de tipo element
+
+    **/
 private:
-std::priority_queue<pair<element,int>> cola;
+/**
+ * @brief cola con prioridad que almacena pares de element e int y que usa el struct Comparar para su prioridad
+ */
+std::priority_queue<pair<element,int>,vector<pair<element,int>>,Comparar> cola;
+
 public:
 
+/**
+ * @brief Constructor sin parámetros que genera una cola con prioridad vacía
+ * @return MaxStack, el objeto MaxStack creado
+ */
 MaxStack();
 
+/**
+ * @brief Compara el elemento a insertar con el máximo hasta ese momento e inserta un element en la pila
+ * @param elemento Entero a introducir en la pila
+ */
 void push(int elemento);
 
-element& top();
+/**
+ * @brief Método para consultar el tope de la pila
+ * @return Referencia constante al element en el tope de la pila
+ */
+const element& top();
 
+/**
+ * @brief Elimina el elemento en el tope de la pila
+ */
 void pop();
 
+/**
+ * @brief Método para consultar si la pila está vacía
+ * @return Bool que nos indica si la pila está vacía, true en caso afirmativo y false en el contrario.
+ */
 bool empty();
 
+/**
+ * @brief Método para consultar el tamaño de la pila
+ * @return Entero que nos indica cuántos elementos hay introducidos en la pila
+ */
 int size();
 };
 #endif //PRACTICA4_MAXSTACK
